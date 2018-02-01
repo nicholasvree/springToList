a1d20189f708fbb535f8785a318170c4ff31d7c5
Nicholas Vree <nicholasvree@Nicholass-MacBook-Air.local>
2018-02-01 10:09:57 -0600

    first commit

---
+++ src/main/java/com/in28minutes/todo/TodoController.java
@@ -0,0 +1,66 @@
+package com.in28minutes.todo;
+
+import java.util.Date;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.in28minutes.model.Todo;
+import com.in28minutes.todo.service.TodoService;
+
+@Controller
+@SessionAttributes("name")
+public class TodoController {
+
+	@Autowired
+	private TodoService service;
+
+	@RequestMapping(value = "/list-todos", method = RequestMethod.GET)
+	public String showTodosList(ModelMap model) {
+		String user = (String) model.get("name");
+		model.addAttribute("todos", service.retrieveTodos(user));
+		return "list-todos";
+	}
+
+	@RequestMapping(value = "/add-todo", method = RequestMethod.GET)
+	public String showAddTodoPage(ModelMap model) {
+		model.addAttribute("todo", new Todo());
+		return "todo";
+	}
+
+	@RequestMapping(value = "/add-todo", method = RequestMethod.POST)
+	public String addTodo(ModelMap model, @Valid Todo todo, BindingResult result) {
+
+		if (result.hasErrors())
+			return "todo";
+
+		service.addTodo((String) model.get("name"), todo.getDesc(), new Date(),
+				false);
+		model.clear();// to prevent request parameter "name" to be passed
+		return "redirect:/list-todos";
+	}
+
+	@RequestMapping(value = "/update-todo", method = RequestMethod.GET)
+	public String updateTodo(ModelMap model, @RequestParam int id) {
+		Todo todo = service.retrieveTodo(id);
+		model.addAttribute("todo", todo);
+
+		return "todo";
+	}
+	
+	@RequestMapping(value = "/delete-todo", method = RequestMethod.GET)
+	public String deleteTodo(@RequestParam int id) {
+		service.deleteTodo(id);
+
+		return "redirect:/list-todos";
+	}
+
+}
